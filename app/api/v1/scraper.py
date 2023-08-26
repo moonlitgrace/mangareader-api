08ea58389c949dfdc0989b644f7badeb39c52a8e
tokitou-san <tokitousan2005@gmail.com>
2023-08-26 20:34:39 +0530

    refactor: used single function to scrape

--- app/api/v1/scraper.py
+++ app/api/v1/scraper.py
@@ -1,58 +1,54 @@
 import requests
-from bs4 import BeautifulSoup, element
+from bs4 import BeautifulSoup
+
+def isfloat(str: str) -> bool:
+    try:
+        float(str)
+        return True
+    except ValueError: return False
 
 # Scrape popular mangas
 class PopularManagasScraper:
     def __init__(self):
         self.URL = "https://mangareader.to/home"
 
+    def _scrape_text(self, element, selector):
+        selected_element = element.select_one(selector)
+        return selected_element.text.strip() if selected_element else None
+
+    def _scrape_numeric(self, element, selector):
+        selected_value = self._scrape_text(element, selector)
+        print(selected_value)
+        if selected_value:
+            for value in selected_value.split():
+                if isfloat(value) or value.isdigit(): return float(value)
+        return None
+
     def _scrape_ranking(self, element):
-        ranking_container = element.find("div", class_="number")
-        rank = ranking_container.find("span")
-        if rank:
-            return float(rank.text)
-        return "Ranking not found"
+        ranking = self._scrape_text(element, ".number span")
+        return int(ranking) if ranking else None
 
     def _scrape_title(self, element):
-        title_element = element.find("div", class_="anime-name")
-        if title_element:
-            return title_element.text.strip()
-        return "No title found"
-
-    def _scrape_image(self, element):
-        cover = element.find("img", class_="manga-poster-img")
-        if cover:
-            return cover["src"]
-        return "Cover image not found"
-
-    def _scrape_rating(self, element):
-        container = element.find("div", class_="mp-desc")
-        rating = container.find_all("p")[1].text
-        if rating:
-            return float(rating)
-        return "Rating not found"
-
-    def _scrape_chapters(self, element):
-        container = element.find("div", class_="mp-desc")
-        chapters_string = container.find_all("p")[3].text
-        if chapters_string:
-            for string in chapters_string.split():
-                if string.isdigit(): return int(string)
-        return "Chapters not found"
-
-    def _scrape_volumes(self, element):
-        container = element.find("div", class_="mp-desc")
-        volumes_string = container.find_all("p")[4].text
-        if volumes_string:
-            for string in volumes_string.split():
-                if string.isdigit(): return int(string)
-        return "Volumes not found"
+        return self._scrape_text(element, ".anime-name")
 
     def _scrape_link(self, element):
         link = element.find("a", class_="link-mask")["href"]
         slug = link.replace("/", "")
-        if slug: return slug
-        return "Slug not found"
+        return slug if slug else None
+
+    def _scrape_image(self, element):
+        cover = element.find("img", "manga-poster-img")
+        return cover["src"] if cover else None
+
+    def _scrape_rating(self, element):
+        rating = self._scrape_text(element, ".mp-desc p:nth-of-type(2)")
+        return float(rating) if rating else None
+
+    def _scrape_chapters(self, element):
+        return self._scrape_numeric(element, ".mp-desc p:nth-of-type(4)")
+
+    def _scrape_volumes(self, element):
+        return self._scrape_numeric(element, ".mp-desc p:nth-of-type(5)")
 
     def scrape(self):
         data = []
