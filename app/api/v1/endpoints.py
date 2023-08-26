d376ac6c38fd153b3e850c5b0ad034c5eb8148b3
tokitou-san <tokitousan2005@gmail.com>
2023-08-26 16:01:15 +0530

    add

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter
+from bs4 import BeautifulSoup
 
 router = APIRouter()
+
+BASE_URL = "https://mangareader.to/home"
 
 @router.get("/")
 async def root():
 	return { "message": "MangaAPI V1 API" }
+
+@router.get("/popular")
+async def get_trending_mangas():
+	soup = BeautifulSoup(BASE_URL)
+	print(soup)
+	return None
