47dc5d51824f9be0cf7e2280413d82d029c83339
tokitou-san <tokitousan2005@gmail.com>
2023-08-27 13:08:10 +0530

    feat: add get slug and cover function

--- app/api/v1/scrapers/popular_mangas.py
+++ app/api/v1/scrapers/popular_mangas.py
@@ -1,5 +1,5 @@
 import requests
-from selectolax.parser import HTMLParser
+from selectolax.parser import HTMLParser, Node
 
 class PopularScraper:
 	def __init__(self) -> None:
@@ -16,19 +16,25 @@ class PopularScraper:
 		return HTMLParser(res.content)
 
 	@staticmethod
-	def __get_title(element):
+	def __get_title(element: Node):
 		title = element.css_first(".anime-name").text()
 		return title if title else None
 
 	@staticmethod
-	def __get_slug(element):
-		slug = element.css_first("a.link-mask").attrs["href"].replace("/", "")
-		return slug if slug else None
+	def __get_slug(element: Node):
+		slug = element.css_first("a.link-mask").attributes["href"]
+		return slug.replace("/", "") if slug else None
+
+	@staticmethod
+	def __get_cover(element: Node):
+		cover = element.css_first("img.manga-poster-img").attributes["src"]
+		return cover if cover else None
 
 	def __build_dict(self, element):
 		manga_dict = {
 			"title": self.__get_title(element),
-			"slug": self.__get_slug(element)
+			"slug": self.__get_slug(element),
+			"cover": self.__get_cover(element)
 		}
 
 		return manga_dict
