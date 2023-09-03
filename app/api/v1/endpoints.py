d81c156a24f430f7d8958ad67ff3ee09096e7750
tokitou-san <tokitousan2005@gmail.com>
2023-09-03 22:08:05 +0530

    Fix syntax

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,5 +1,4 @@
 from dataclasses import astuple
-from typing import List
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
@@ -9,13 +8,16 @@ from .scrapers.topten import TopTenScraper
 from .scrapers.most_viewed import MostViewedScraper
 from .scrapers.manga import MangaScraper
 # models
-from .models import PopularMangaModel, BaseModel, TopTenMangaModel, MostViewedMangaModel, MangaModel
+from .models import (
+	PopularMangaModel,
+	BaseModel,
+	TopTenMangaModel,
+	MostViewedMangaModel,
+	MangaModel
+)
 
+# router
 router = APIRouter()
-
-@router.get("/", include_in_schema=False)
-async def root():
-	return { "message": "MangaAPI V1 API" }
 
 @router.get("/popular", response_model=list[PopularMangaModel])
 async def get_popular():
@@ -32,8 +34,7 @@ async def get_most_viewed(chart: str):
 	most_viewed_scraper = MostViewedScraper()
 
 	if chart in most_viewed_scraper.CHARTS:
-		response = most_viewed_scraper.parse(chart)
-		return response
+		return most_viewed_scraper.parse(chart)
 	else:
 		error = f"{chart} not in {', '.join(most_viewed_scraper.CHARTS)}"
 		message = f"Passed query ({chart}) is invalid. Valid queries {' | '.join(most_viewed_scraper.CHARTS)}"
@@ -52,5 +53,4 @@ async def get_most_viewed(chart: str):
 
 @router.get("/manga/{slug}", response_model=MangaModel)
 async def get_manga(slug: str):
-	response = MangaScraper(slug).parse()
-	return response
+	return MangaScraper(slug).parse()
