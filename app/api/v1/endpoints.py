1103eea6edb5dc943b3fc241e9b53f3bef9368c9
tokitou-san <tokitousan2005@gmail.com>
2023-09-04 13:10:17 +0530

    refactor: endpoints query limit and exception

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,5 +1,5 @@
 from dataclasses import astuple
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import JSONResponse
 
 # decorators
@@ -24,32 +24,31 @@ router = APIRouter()
 # get popular/trending mangas list
 @router.get("/popular", response_model=list[PopularMangaModel])
 @handle_exceptions("Something went wrong, please try again!", 503)
-async def get_popular(skip: int = 0, limit: int = 10):
+async def get_popular(offset: int = 0, limit: int = Query(10, lt=10)):
 	response = PopularScraper().parse()
-	return response[skip: skip+limit]
+	return response[offset: offset+limit]
 
 # get top 10 mangas list
 @router.get("/top-10", response_model=list[TopTenMangaModel])
 @handle_exceptions("Something went wrong, please try again!", 503)
-async def get_top_ten(skip: int = 0, limit: int = 10):
+async def get_top_ten(offset: int = 0, limit: int = Query(10, lt=10)):
 	response = TopTenScraper().parse()
-	return response[skip: skip+limit]
+	return response[offset: offset+limit]
 
 # get most viewed mangas list by chart (dynamic)
 @router.get("/most-viewed/{chart}", response_model=list[MostViewedMangaModel])
+@handle_exceptions("Something went wrong, please try again!", 503)
 async def get_most_viewed(chart: str):
 	most_viewed_scraper = MostViewedScraper()
 
 	if chart in most_viewed_scraper.CHARTS:
 		return most_viewed_scraper.parse(chart)
 	else:
-		error = f"{chart} not in {', '.join(most_viewed_scraper.CHARTS)}"
 		message = f"Passed query ({chart}) is invalid. Valid queries {' | '.join(most_viewed_scraper.CHARTS)}"
 		status_code = 400
 
 		raise HTTPException(
 			detail = {
-				"error": error,
 				"message": message,
 				"status_code": status_code
 			},
