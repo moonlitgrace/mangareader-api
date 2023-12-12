f3eac3f924ca47172ebbba16cdab7d3cd9d00e41
tokitouq <tokitousan2005@gmail.com>
2023-12-12 17:24:36 +0530

    feat: add error handling for getting provider

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from providers import providers_urls, providers_css_selectors
 
 router = APIRouter()
@@ -7,6 +7,9 @@ router = APIRouter()
 @router.get(path="/{provider}/manga/{id}/{title}")
 async def manga(provider: str, id: int, title: str):
     provider_url = providers_urls.get(provider)
+    if not provider_url:
+        raise HTTPException(404, detail="Provider not found!")
+
     manga_url = f"{provider_url}/{id}/{title}/"
     css_selectors = providers_css_selectors.get(provider).get("manga")
     return css_selectors
