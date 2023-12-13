4b4924a961d1d6115135a01edf0cf8e802e21e90
tokitouq <tokitousan2005@gmail.com>
2023-12-13 23:09:10 +0530

    fix: search scraper

--- providers/mangareader/search.py
+++ providers/mangareader/search.py
@@ -3,4 +3,10 @@ from typing import Dict
 css_selectors: Dict = {
     "nodes_wrapper": "#main-content .mls-wrap .item",
     "title": ".manga-detail .manga-name a",
+    "slug": ".manga-detail .manga-name a",
+    "genres": ".manga-detail .fd-infor .fdi-item a",
+    "langs": ".manga-poster .tick-lang",
+    "cover_src": ".manga-poster img",
+    "chapters": ".manga-detail .fd-list .fdl-item .chapter a",
+    "volumes": ".manga-detail .tab-pane:nth-child(2) .fd-list .fdl-item .chapter a",
 }
--- server/scrapers/search.py
+++ server/scrapers/search.py
@@ -1,3 +1,4 @@
+import re
 from typing import Dict
 from selectolax.parser import Node
 from ..helpers import HTMLHelper, StringHelper
@@ -13,19 +14,30 @@ class SearchMangaScraper:
 
     def __get_item(self, node_main: Node, property_name: str):
         node = node_main.css_first(self.css_selectors.get(property_name, ""))
+        if not node:
+            return None
         node_text = node.text(strip=True)
         return node_text.split(":")[1] if ":" in node_text else node_text
 
     def __get_item_int(self, node_main: Node, property_name: str):
         item = self.__get_item(node_main, property_name)
-        return float(item) if item else None
+        if not item:
+            return None
+        # if item is a string with int
+        try:
+            int(item)
+        except Exception:
+            nums = re.findall(r"\d+", item)
+            return float(nums[0])
+        # normal
+        return float(item)
 
     def __get_item_list(self, node_main: Node, property_name: str):
         nodes = node_main.css(self.css_selectors.get(property_name, ""))
         return [node.text(strip=True) for node in nodes] if nodes else []
 
-    def __get_item_attr(self, property_name: str, attr: str):
-        node = self.parser.css_first(self.css_selectors.get(property_name, ""))
+    def __get_item_attr(self, node_main: Node, property_name: str, attr: str):
+        node = node_main.css_first(self.css_selectors.get(property_name, ""))
         return node.attributes[attr] if node else None
 
     def scrape(self):
@@ -35,6 +47,12 @@ class SearchMangaScraper:
         for node in nodes:
             manga_dict = {
                 "title": self.__get_item(node, "title"),
+                "slug": self.__get_item_attr(node, "slug", "href"),
+                "genres": self.__get_item_list(node, "genres"),
+                "langs": self.__get_item(node, "langs"),
+                "cover_src": self.__get_item_attr(node, "cover_src", "src"),
+                "chapters": self.__get_item_int(node, "chapters"),
+                "volumes": self.__get_item_int(node, "volumes"),
             }
 
             mangas_list.append(manga_dict)
