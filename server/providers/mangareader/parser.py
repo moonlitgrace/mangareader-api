ccf3dd2b62bf9431ea6074dff3bc1bca7aa986a6
tokitouq <tokitousan2005@gmail.com>
2023-12-16 09:36:15 +0530

    fix: get_manga endpoint

--- server/providers/mangareader/parser.py
+++ server/providers/mangareader/parser.py
@@ -1,6 +1,5 @@
 from ...helpers import HTMLHelper
 
-
 class MangaReader:
     def __init__(self):
         self.html_helper = HTMLHelper()
@@ -11,7 +10,15 @@ class MangaReader:
         _ = self.html_helper.get_parser(parse_url)
 
         manga_dict = {}
-        manga_dict["title"] = _.css_first(
-            ".anisc-detail .manga-name").text(strip=True)
+        manga_dict["title"] = _.css_first(".anisc-detail .manga-name").text(strip=True)
+        manga_dict["slug"] = query
+        manga_dict["alt_title"] = _.css_first(".anisc-detail .manga-name-or").text(strip=True)
+        manga_dict["type"] = _.select("span").text_contains("Type:").matches[0].parent.css_first(".name").text(strip=True).lower()
+        manga_dict["status"] = _.select("span").text_contains("Status:").matches[0].parent.css_first(".name").text(strip=True).lower()
+        manga_dict["authors"] = _.select("span").text_contains("Authors:").matches[0].parent.css_first("a").text(strip=True)
+        manga_dict["genres"] = [genre.text(strip=True).lower() for genre in _.css(".anisc-detail .genres a")]
+        manga_dict["score"] = _.select("span").text_contains("Score:").matches[0].parent.css_first(".name").text(strip=True)
+        manga_dict["cover"] = _.css_first(".anisc-poster .manga-poster-img").attrs.get("src", "")
+        manga_dict["synopsis"] = _.css_first(".modal-content .description-modal").text(strip=True)
 
         return manga_dict
