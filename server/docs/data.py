6ce67f75fe2825d9fb07e46b0aa30fe12d1139f8
tokitouq <tokitousan2005@gmail.com>
2023-12-18 20:29:24 +0530

    fix: add basic sidebar logic

---
+++ server/docs/data.py
@@ -0,0 +1,10 @@
+sidebar_mapping = {
+    "MangaKomi": {
+        "get manga info": "/mangakomi/manga/",
+        "search": "/mangakomi/search/",
+    },
+    "MangaReader": {
+        "get manga info": "/mangareader/manga",
+        "search": "/mangareader/search",
+    },
+}
--- server/docs/routes.py
+++ server/docs/routes.py
@@ -2,23 +2,20 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from .data import sidebar_mapping
+
 router = APIRouter()
 
 # https://fastapi.tiangolo.com/advanced/templates/
 templates = Jinja2Templates(directory="client/templates/")
 
-def get_app():
-    from server.api import app
-    return app
-
 # homepage route
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def index(request: Request):
-    openapi = get_app().openapi()
     return templates.TemplateResponse(
         "index.html",
         context={
             "request": request,
-            "openapi": openapi,
+            "sidebar_mapping": sidebar_mapping,
         },
     )
