4e133f97e2500f95d2e9f46e697b7fc8b4db6fa9
tokitouq <tokitousan2005@gmail.com>
2023-12-18 15:03:52 +0530

    feat: add search endpoint for mangakomi

--- server/providers/mangakomi/parsers/__init__.py
+++ server/providers/mangakomi/parsers/__init__.py
@@ -1,1 +1,2 @@
 from .manga import MangaParser
+from .search import SearchParser
--- server/providers/mangakomi/routes.py
+++ server/providers/mangakomi/routes.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
-
-from .parsers import MangaParser
+from server.decorators import return_on_404
+from .parsers import MangaParser, SearchParser
 
 router = APIRouter()
 
 
 @router.get("/manga/{query}")
+@return_on_404()
 async def manga(query: str):
     manga_parser = MangaParser(query)
     return manga_parser.build_dict()
+
+@router.get("/search/{query}")
+@return_on_404()
+async def search(query: str):
+    search_parser = SearchParser(query)
+    return search_parser.build_list()
