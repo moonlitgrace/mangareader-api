a0f11570339c2b074e1ac3c6cb3b836aebd42f3a
tokitouq <suneeth.since2005@gmail.com>
2024-04-29 22:31:09 +0530

    fix: project setup script with poetry support

--- setup.sh
+++ setup.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-VENV = "venv"
+# Poetry is must for this script
+read -p "Do you have poetry installed? (y/n)" poetry_yn
 
-# create venv if its not created
-if [! -d "${VENV}"]; then
-	echo "Creating virtual environment..."
-	python3 -m venv "${VENV}"
+if [[ $poetry_yn == "y" || $poetry_yn == "Y" ]] then
+	# install poetry deps
+	echo "Installing project dependencies using Poetry..."
+	poetry install
+
+	# run server
+	echo "Starting server..."
+	poetry run python3 main.py
+
+	while true; do
+		read -p "Do you want to start server? (y/n)" start_yn
+
+		case $start_yn in
+			[Yy]* )
+				echo "Server starting...";
+				poetry run python3 main.py;;
+
+			[Nn]* )
+				echo "Exiting...";
+				echo "you can run server later using the command:";
+				echo "poetry run python3 main.py";
+				exit;;
+			* ) echo "Please answer with Y/y/N/n.";;
+
+		esac
+	done
+
+else
+	echo "Sorry, this script only works with poetry";
+	echo "Please install it and re-run this script";
+	exit;
 fi
-
-# activate venv
-source "${VENV}"/bin/activate
-
-# install poetry deps
-echo "Installing project dependencies using Poetry..."
-poetry install
