7358820a1ecf35f4af9b633115e30438c488b389
tokitouq <suneeth.since2005@gmail.com>
2024-05-01 20:23:21 +0530

    fix: bash command with new update

--- setup.sh
+++ setup.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Poetry is must for this script
-read -p "Do you have poetry installed? (y/n)" poetry_yn
+read -p "Do you have poetry installed? (y/n): " poetry_yn
 
 if [[ $poetry_yn == "y" || $poetry_yn == "Y" ]] then
 	# install poetry deps
 	echo "Installing project dependencies using Poetry..."
 	poetry install
 
-	while true; do
-		read -p "Do you want to start server? (y/n)" start_yn
+	read -p "Do you want to start server? (y/n): " start_yn
 
-		case $start_yn in
-			[Yy]* )
-				echo "Server starting...";
-				poetry run dev;;
+	case $start_yn in
+		[Yy]* )
+			echo "Server starting...";
+			poetry run dev;;
 
-			[Nn]* )
-				echo "Exiting...";
-				echo "you can run server later using the command:";
-				echo "poetry run dev";
-				exit;;
-			* ) echo "Please answer with Y/y/N/n.";;
+		[Nn]* )
+			echo "Exiting...";
+			echo "you can run server later using the command:";
+			echo "poetry run dev";
+			exit;;
+		* ) exit 0;;
 
-		esac
-	done
+	esac
 
 else
+	echo ""
 	echo "Sorry, this script only works with poetry";
 	echo "Please install it and re-run this script";
-	exit;
+	echo ""
+	exit 0;
 fi
