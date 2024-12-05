cb2debe833b9fe8adfd520cd7c255111197a3312
ilovemoon <moonlitgrace.gaia@gmail.com>
2024-12-05 09:32:24 +0530

    feat: add `cors` (#495)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -29,6 +30,21 @@ app = FastAPI(
     # disable redoc and swagger
     redoc_url=None,
     docs_url=None,
+)
+
+# cors
+# https://fastapi.tiangolo.com/tutorial/cors/#use-corsmiddleware
+origins = [
+    # specify allowed origins
+    "*"  # if specified remove this lint
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # https://stackoverflow.com/a/61644963/20547892
